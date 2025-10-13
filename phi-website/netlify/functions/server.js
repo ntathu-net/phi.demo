7fe8c01b12d8593aab140606fa7658a53bfab1c4
cyneweard-zw <bongani@ntathu.net>
2025-10-13 14:47:16 +0200

    initi

---
+++ phi-website/netlify/functions/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const serverless = require('serverless-http');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Route for home page using EJS
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+// Example route for another page
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+module.exports = app;
+module.exports.handler = serverless(app);
---
+++ phi-website/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route for home page using EJS
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Home' });
+});
+
+// Example route for another page
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About' });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running at http://localhost:${port}`);
+});
