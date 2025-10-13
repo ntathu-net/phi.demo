e19f3f73addadbc2abf6191cdc35adea41fcebea
cyneweard-zw <bongani@ntathu.net>
2025-10-13 15:25:50 +0200

    Update build.js

--- phi-website/build.js
+++ phi-website/build.js
@@ -5,6 +5,11 @@ const path = require('path');
 const viewsDir = path.join(__dirname, 'views');
 const distDir = path.join(__dirname, 'dist');
 
+// Set EJS views directory
+ejs.fileLoader = (filePath) => {
+    return fs.readFileSync(path.join(viewsDir, filePath), 'utf8');
+};
+
 // Ensure dist directory exists
 if (!fs.existsSync(distDir)) {
     fs.mkdirSync(distDir, { recursive: true });
@@ -21,11 +26,11 @@ const files = ['index', 'about'];
 
 files.forEach(file => {
     const ejsPath = path.join(viewsDir, `${file}.ejs`);
-    const htmlPath = path.join(distDir, `${file}.html`);
 
     const template = fs.readFileSync(ejsPath, 'utf8');
     const html = ejs.render(template, { title: file === 'index' ? 'Home' : 'About' });
 
+    const htmlPath = path.join(distDir, `${file}.html`);
     fs.writeFileSync(htmlPath, html);
 });
 
