9d4698c8004a5a38c49f5482eb4ca3a6afb3f415
cyneweard-zw <bongani@ntathu.net>
2025-10-15 13:55:00 +0200

    Update server.js
    Logofix 5

--- phi-website/netlify/functions/server.js
+++ phi-website/netlify/functions/server.js
@@ -8,6 +8,10 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // Serve static files
+app.use((req, res, next) => {
+  console.log(`Request: ${req.method} ${req.url}`);
+  next();
+});
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Route for home page using EJS
